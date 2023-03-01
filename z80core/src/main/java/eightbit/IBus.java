ee89d4408880cde4bb6dada8f15d06667ed14eb4
David Conneely <david.conneely@microfocus.com>
2023-03-01 22:33:33 +0000

    Minor updates

--- z80core/src/main/java/eightbit/IBus.java
+++ z80core/src/main/java/eightbit/IBus.java
@@ -3,14 +3,14 @@ package eightbit;
 public interface IBus {
     /**
      * read from memory during the M1 machine cycle which will be the op code of the next instruction.
-     * @param 16 bit memory address.
+     * @param address 16-bit memory address.
      * @return 8 bits of data (the op code).
      */
     byte readOpCode(short address);
 
     /**
      * read from memory of data (i.e. not during the M1 machine cycle of an instruction).
-     * @param 16 bit memory address.
+     * @param address 16-bit memory address.
      * @return 8 bits of data.
      */
     byte readMemory(short address);
